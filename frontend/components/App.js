80fd44255ba6e4b38f8fc3d5cbab141fe1eea6e1
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-06-19 22:06:19 -0700

    project in progress2

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,16 +14,17 @@ export default class App extends React.Component {
     const { value } = evt.target
     this.setState({ ...this.state, todoNameInput: value })
   }
+  resetForm = () =>  this.setState({ ...this.state, todoNameInput:''})
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.reponse.data.message })
   // use the the syntax below for any elements created on your own (componentDidMount is a react function different), 
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
       .then(res => {
+        // check out video could be done differently
         this.fetchAllTodos()
-        this.setState({ ...this.state, todoNameInput:''})
+        this.resetForm()
       })
-      .catch(err => {
-        this.setState({ ...this.state, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
 
   onTodoFormSubmit = e => {
@@ -36,9 +37,21 @@ export default class App extends React.Component {
       .then(res =>{
         this.setState({ ...this.state, todos: res.data.data })
       })
-      .catch(err => {
-        this.setState({ ...this.state, error: err.reponse.message})
+      .catch(this.setAxiosResponseError)
+  }
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+      // this.fetchAllTodos() // adtional way to make the function work for the completed checkmark (not as eficient tho).
+         this.setState({ 
+          ...this.state, todos: this.state.todos.map(todo =>{
+           if (todo.id !== id) return todo
+          return res.data.data
+        })
+       }
+       )
       })
+      .catch(this.setAxiosResponseError)
   }
   componentDidMount() {
     // fetch from server (todos); 
@@ -52,7 +65,7 @@ export default class App extends React.Component {
             <h2>Todos</h2>
             {
               this.state.todos.map(todo => {
-                return <div key={todo.id}>{todo.name}</div>
+                return <div onClick={this.toggleCompleted(todo.id)} key={todo.id}> {todo.name}{todo.completed ? '✔️' : ''}</div>
               })
             }
           </div>
